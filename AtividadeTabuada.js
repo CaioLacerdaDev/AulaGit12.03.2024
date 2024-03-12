c6afd62826c9a97217918819889303a64bb73c3b
CaioLacerdaDev <caiolacerda.tds1.senai@gmail.com>
2024-03-12 09:28:50 -0300

    Alterei a variavel tabuada para conta no arquivo AtividadeTabuada.js

--- AtividadeTabuada.js
+++ AtividadeTabuada.js
@@ -1,16 +1,16 @@
 let limite = 834;
-let tabuada = 2092;
+let conta = 2092;
 
 
 if (limite <= 0) {
     console.log("não podemos multiplicar por numero negativos e igual zero")
 }
-else if (tabuada % 1 !== 0 ){
+else if (conta % 1 !== 0 ){
     console.log ("não multiplicamos numero com virgula")
 }else {
 
-    for (let i = 1; i <= tabuada; i++) {
-        console.log(tabuada + " x "  + i + " = " + tabuada * i);
+    for (let i = 1; i <= conta; i++) {
+        console.log(conta + " x "  + i + " = " + conta * i);
     }
 }
 
